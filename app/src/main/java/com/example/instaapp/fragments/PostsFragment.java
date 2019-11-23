cef10779b2a242538b92ac082206302cfa55ea69
Priyil <primzray@gmail.com>
2019-11-23 02:51:21 -0500

    Added Walthrough

--- app/src/main/java/com/example/instaapp/fragments/PostsFragment.java
+++ app/src/main/java/com/example/instaapp/fragments/PostsFragment.java
@@ -11,6 +11,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.example.instaapp.Post;
 import com.example.instaapp.PostsAdapter;
@@ -26,6 +27,7 @@ public class PostsFragment extends Fragment {
 
     public final String TAG="PostsFragment";
     private RecyclerView rvPosts;
+    SwipeRefreshLayout swipeContainer;
     protected PostsAdapter adapter;
     protected List<Post> mPosts;
 
@@ -34,7 +36,26 @@ public class PostsFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_posts,container, false);
 
-        
+        super.onCreate(savedInstanceState);
+        // Only ever call `setContentView` once right at the top
+        setContentView(R.layout.item_post);
+        // Lookup the swipe container view
+        swipeContainer = (SwipeRefreshLayout) view.findViewById(R.id.swipeContainer);
+        // Setup refresh listener which triggers new data loading
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                // Your code to refresh the list here.
+                // Make sure you call swipeContainer.setRefreshing(false)
+                // once the network request has completed successfully.
+                queryPosts();
+            }
+        });
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
     }
 
     @Override
